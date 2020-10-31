0ca435760f2f10514d3434d294702d554cd31bb9
Erika <erika@beasleys.com>
2020-10-31 11:13:13 -0400

    adding Components

--- lessonplan/src/App.js
+++ lessonplan/src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Main from './components/Main';
+import Wrapper from './components/Wrapper';
+import Nav from './components/Nav';
+import "./App.css";
+import LoginPage from './components/LoginPage';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   <Wrapper>
+     <Nav />
+     <LoginPage />
+   </Wrapper>
     </div>
   );
 }
 
 export default App;
+
+
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
---
+++ lessonplan/src/components/Nav/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import "./Nav.css";
+
+function Nav() {
+    return (
+        <nav className="navbar navbar-light bg-light">
+            <a class="navbar-brand" href="#">
+                {/* <img src="#" width="30" height="30" class="d-inline-block align-top" alt=""></img> */}
+                    Bootstrap
+            </a>
+        </nav>
+      
+    );
+}
+export default Nav;
